cd8ab0511b083316cc16950aee3a5f18ae1c23b9
David Fernandes <david@icomp.ufam.edu.br>
2019-11-07 12:05:04 -0400

    Uso de expressoes e constructor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Penelope'
+    }
+  }
+
   render = () =>
   <div>
     <h4 className="bg-primary text-white text-center p-2">
-      To Do List
+      {this.state.name}'s To Do List
     </h4>
   </div>
 }
