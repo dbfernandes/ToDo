066e9aaf88ba386070c054957f933caf7154b501
David Fernandes <david@icomp.ufam.edu.br>
2019-11-07 12:36:24 -0400

    Adição de novas tarefas

--- src/App.js
+++ src/App.js
@@ -5,23 +5,41 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      nome: 'Penelope'
+      name: "Ricardo",
+      tasks: [
+        { action: "Comprar flores", done: false },
+        { action: "Estudar para a prova", done: false },
+        { action: "Ligar para a Maria", done: true },
+        { action: "Limpar a casa", done: false }
+      ],
+      newTask: ""
     }
   }
 
-  changeName = () => {
+  updateNewTaskAction = (e) => {
     this.setState({
-      nome: (this.state.nome === 'Penelope')?'Ricardo':'Penelope'
+      newTask: e.target.value
+    })
+  }
+
+  createNewTask = () => {
+    this.setState({
+      tasks: [...this.state.tasks, { action: this.state.newTask, done: false }],
+      newTask: ''
     })
   }
 
   render = () =>
-  <div>
-    <h4 className="bg-primary text-white text-center p-2">
-      {this.state.nome}'s To Do List
-    </h4>
-    <button onClick={this.changeName} className="btn btn-primary">Mudar Nome</button>
-  </div>
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        {this.state.name}'s To Do List &mdash; Tarefas a fazer:&nbsp;
+        {this.state.tasks.filter(t => !t.done).length}
+      </h4>
+      <div className="container-fluid">
+        <input onChange={this.updateNewTaskAction} value={this.state.newTask} className="form-control" />
+        <button onClick={this.createNewTask} className="btn btn-primary mt-1">Adicionar</button>
+      </div>
+    </div>
 }
 
 export default App
